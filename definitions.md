bdc765f0bacccd3ab785a91c9b808eaeca6baef8
Akash <akash@bwowser.com>
2022-03-05 17:33:37 -0500

    init

---
+++ definitions.md
@@ -0,0 +1,6 @@
+## Tags
+
+> this is a work in progress. DO NOT USE THIS DEFINITION
+1. promax-include src="path-relative-to-root-index.html"
+2. promax-compose controller="ctx.controllerName"
+        <p> promax-compose can have childer, which will be passed as argument to compose-controller</p>
---
+++ script.js
@@ -0,0 +1,58 @@
+var htmlPromax = {};
+(() => {
+  const includeTree = {};
+  const dupeCheck = (key) => !includeTree[key];
+  function depthBreak(depth) {
+    if (depth > 99) {
+      throw new Error("Maximum include depth reached!!");
+    }
+  }
+  function makeKey(dupeCheck, depth = 0) {
+    depthBreak(depth);
+    const key = btoa(Math.random().toString()).substring(2, 10);
+    if (dupeCheck(key)) return key;
+    return makeKey(dupeCheck, depth + 1);
+  }
+  function bindKey(elmnt, key, filePath) {
+    // const [,...path]=filePath.replace(/^(.*)\/$/, '$1').split('/');
+    elmnt.setAttribute("included", key);
+    includeTree[key] = { src: filePath, key }; // not useful now
+  }
+
+  // copied from here >> https://www.w3schools.com/howto/howto_html_include.asp
+  function includeHTML(count = 0) {
+    depthBreak(count);
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    elements = document.getElementsByTagName("include");
+    Array.from(elements).forEach((elmnt) => {
+      if (elmnt.getAttribute("included")) return;
+      /*search for elements with a certain atrribute:*/
+      file = elmnt.getAttribute("src");
+      if (file) {
+        /* Make an HTTP request using the attribute value as the file name: */
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+          if (this.readyState == 4) {
+            if (this.status == 200) {
+              elmnt.innerHTML = this.responseText;
+            }
+            if (this.status == 404) {
+              throw new Error("Page not found.");
+            }
+            /* Remove the attribute, and call this function once more: */
+            //   elmnt.removeAttribute("w3-include-html");
+            bindKey(elmnt, makeKey(dupeCheck), file);
+            includeHTML(count + 1);
+          }
+        };
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /* Exit the function: */
+        return;
+      }
+    });
+  }
+
+  htmlPromax.start = includeHTML;
+})();
